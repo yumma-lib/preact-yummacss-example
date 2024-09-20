0f5a4eec2c5f4201c59fcea0cf057f1fc8aee843
Renildo Pereira <rrenildopereiraa@gmail.com>
2024-09-20 18:44:30 +0100

    add files

---
+++ src/index.tsx
@@ -0,0 +1,26 @@
+import { LocationProvider, Router, Route, hydrate, prerender as ssr } from 'preact-iso';
+
+import { Home } from './pages/Home/index.jsx';
+import { NotFound } from './pages/_404.jsx';
+import './style.css';
+
+export function App() {
+	return (
+		<LocationProvider>
+			<main>
+				<Router>
+					<Route path="/" component={Home} />
+					<Route default component={NotFound} />
+				</Router>
+			</main>
+		</LocationProvider>
+	);
+}
+
+if (typeof window !== 'undefined') {
+	hydrate(<App />, document.getElementById('app'));
+}
+
+export async function prerender(data) {
+	return await ssr(<App {...data} />);
+}
---
+++ src/pages/Home/index.tsx
@@ -0,0 +1,7 @@
+export function Home() {
+  return (
+    <div class="h-1/1 ins">
+      <h1 class="fs-xxl fw-500 tc-pink">Yumma CSS + Preact</h1>
+    </div>
+  );
+}
